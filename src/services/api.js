020841b86082f297fd67b90efb38ddba90cde579
Leandro Gioria <legioria@hotmail.com>
2019-12-27 12:00:38 -0300

    added eslint-plugin-import-helpers

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.31.51:3333',
+  // baseURL: 'http://192.168.31.51:3333',
+  baseURL: 'http://35.185.63.231:3333',
 });
 
 export default api;
